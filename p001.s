2a063d930715f7aeca22c21ad176302c097d9618
Luis-Miguel-Alvarez <lma6113@gmail.com>
2017-09-14 19:56:01 -0400

    fixed errors

--- p001.s
+++ p001.s
@@ -5,26 +5,40 @@
 **/
 
 .data
+base:
+x: .asciz "0"
+.set offset_of_x, x - base
 
-nine: .asciz "0"
-littleA: .asciz "a"
-BigA: .asciz "A"
+//nine: .asciz "0"
+//littleA: .asciz "a"
+//BigA: .asciz "A"
 
 .text
 
 .global main
 
 main:
-    push {lr} 
-    mov r0, #0
+    
+    push {r9, lr}
+    data_seg .reg r9 
+    ldr data_seg, data_seg_address
+    
+    mov r0, x
+    str r0, [data_seg, #offset_of_x]
 
 while: 
+    ldr r0, [data_seg, #offset_of_x]
     cmp r0, #10
     beq exit
-    //bl printf
-    add r0, #1 
+    
+    bl printf
+    add r0, r0, #1
+    str r0, [data_seg, #offset_of_x]  
     b while
     
 exit:
     pop {lr}
     bx lr
+
+//base
+data_seg_address: .word base
