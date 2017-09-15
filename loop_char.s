c3ebc92101e9aaae627e704752a867da8ec04fb5
Luis-Miguel-Alvarez <lma6113@gmail.com>
2017-09-15 02:57:06 -0400

    fixed errors

--- loop_char.s
+++ loop_char.s
@@ -1,14 +1,18 @@
-/*
-  
- */
-        .data
+/*Luis Alvarez
+CIS 356
+Assignment p001
+Due: 9/15/17
+Program uses loops and segment jumps to print out the digits followed
+followed by the upper and lower case alpabet. Memory is used to store 
+values needed to maintain proper loop flow and printing of chars*/
+
+//data segment sets up space and adrress tracking for the three variables 
+.data
         .align 2
 base:
 x:      .word 0
 comp:   .word 0
-
 str:    .asciz "%c "
-
         .set   offset_of_x, x - base
         .set   offset_of_str, str - base
         .set   offset_of_comp, comp - base
@@ -21,7 +25,8 @@ main:
         ldr     data_seg, data_seg_address
         b setNum
 
-
+//while loop uses the starting and ending variables to control loop flow
+//loads and stores before and after branches ensure that everything is kept track of
 while:  ldr     r0, [data_seg, #offset_of_x]
         ldr     r1, [data_seg, #offset_of_comp]
         cmp     r0, r1
@@ -37,15 +42,17 @@ while:  ldr     r0, [data_seg, #offset_of_x]
 	    str     r0, [data_seg, #offset_of_x]
 
         b       while
-        
+
+//Roundabout way of moving on to next set of chars to print without redundancy of while loop
 setLoop:
         cmp r1, #58
         beq setUpper
-        cmp r1, #91
+        cmp r1, #91 
         beq setLower
         cmp r1, #123
         beq done
-        
+
+//following labels move the while loop onto the next set of chars to print
 setNum:
         add r0, data_seg, #offset_of_str
         mov r1, #10
@@ -75,10 +82,9 @@ setLower:
         mov r0, #123
         str r0, [data_seg, #offset_of_comp]
         b while 
-
-
+        
 done:
         .unreq   data_seg
         pop     {r9, lr}
-        bx      lr                                 /* return from main using lr */
+        bx      lr  /* return from main using lr */
 data_seg_address:   .word base
