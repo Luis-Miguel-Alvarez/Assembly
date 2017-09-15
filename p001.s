25f9656a3135be7626b658c410f4c237c62a6ac8
Luis-Miguel-Alvarez <lma6113@gmail.com>
2017-09-14 20:55:59 -0400

    fixed errors

--- p001.s
+++ p001.s
@@ -5,11 +5,11 @@
 **/
 
 .data
-base:
-x: .word 0
+//base:
+//x: .word 0
 str: .asciz "%c"
-.set offset_of_x, x - base
-.set offset_of_str, str - base
+//.set offset_of_x, x - base
+//.set offset_of_str, str - base
 
 //nine: .asciz "0"
 //littleA: .asciz "a"
@@ -21,27 +21,29 @@ str: .asciz "%c"
 
 main:
     
-    push {r9, lr}
-    data_seg .req r9 
-    ldr data_seg, data_seg_address
+    push {lr}
+    //data_seg .req r9 
+    //ldr data_seg, data_seg_address
     
     mov r0, #0
-    str r0, [data_seg, #offset_of_x]
+    //str r0, [data_seg, #offset_of_x]
 
 while: 
-    ldr r0, [data_seg, #offset_of_x]
+    //ldr r0, [data_seg, #offset_of_x]
     cmp r0, #10
     beq exit
     
     
     mov r1, r0
-    ldr r0, [data_seg, #offset_of_x]
+    ldr r0, =str
+    //ldr r0, [data_seg, #offset_of_x]
     //add r0, data_seg, #offset_of_str
     bl printf
     
-    ldr r0, [data_seg, #offset_of_x]
+    mov r0, r1
+    //ldr r0, [data_seg, #offset_of_x]
     add r0, r0, #1
-    str r0, [data_seg, #offset_of_x]  
+    //str r0, [data_seg, #offset_of_x]  
     b while
     
 exit:
@@ -49,4 +51,4 @@ exit:
     bx lr
 
 //base
-data_seg_address: .word base
+//data_seg_address: .word base
