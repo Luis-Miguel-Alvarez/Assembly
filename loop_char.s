48edcf4d459e77211978e446dd6ba71ef0a61dde
Luis-Miguel-Alvarez <lma6113@gmail.com>
2017-09-14 22:19:08 -0400

    fixed errors

---
+++ loop_char.s
@@ -0,0 +1,50 @@
+/*
+  @file loop_char.s
+
+  Improved to make the calculations of addresses easier to follow
+  (and, hopefully, update). Also moved declaration of x before str so
+  that there only need be one .align
+ */
+        .data
+        .align 2
+base:
+x:      .word 0
+str:    .asciz "Ch: %c\n"
+
+        .set   offset_of_x, x - base
+        .set    offset_of_str, str - base
+
+        .text
+        .global main
+main:
+        push    {r9, lr}
+        data_seg .req r9 // set data_seg as an alias for the register r9
+        ldr     data_seg, data_seg_address
+
+        // x = 13
+        mov     r0, #65
+	str     r0, [data_seg, #offset_of_x]
+
+        // while (x <= 67) {
+while:  ldr     r0, [data_seg, #offset_of_x]
+        cmp     r0, #69
+        bgt     done
+
+        // printf(str, x) - str is address of str; x is value of x
+        add     r0, data_seg, #offset_of_str
+        ldr     r1, [data_seg, #offset_of_x]
+        bl      printf
+
+        // x++;
+        ldr     r0, [data_seg, #offset_of_x]
+        add     r0, r0, #1
+	str     r0, [data_seg, #offset_of_x]
+
+        // }
+        b       while
+
+done:
+        .unreq   data_seg
+        pop     {r9, lr}
+        bx      lr                                 /* return from main using lr */
+data_seg_address:   .word base
