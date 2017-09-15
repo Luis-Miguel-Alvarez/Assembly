1786a74b30a3d33bad2a359ca84e03bdf9ca5be2
Luis-Miguel-Alvarez <lma6113@gmail.com>
2017-09-15 01:22:46 -0400

    fixed errors

--- loop_char.s
+++ loop_char.s
@@ -1,15 +1,12 @@
 /*
-  @file loop_char.s
-
-  Improved to make the calculations of addresses easier to follow
-  (and, hopefully, update). Also moved declaration of x before str so
-  that there only need be one .align
+  
  */
         .data
         .align 2
 base:
 x:      .word 0
-str:    .asciz "\n%c "
+
+str:    .asciz "%c "
 
         .set   offset_of_x, x - base
         .set    offset_of_str, str - base
@@ -20,26 +17,39 @@ main:
         push    {r9, lr}
         data_seg .req r9 // set data_seg as an alias for the register r9
         ldr     data_seg, data_seg_address
-
-        mov     r0, #65
-	    str     r0, [data_seg, #offset_of_x]
+        b setNum
+        //mov     r0, #48
+	    //str     r0, [data_seg, #offset_of_x]
 
 while:  ldr     r0, [data_seg, #offset_of_x]
-        cmp     r0, #90
-        bgt     done
+        cmp     r0, #57
+        
+        
+        bgt     setLower
 
         // printf(str, x) - str is address of str; x is value of x
         add     r0, data_seg, #offset_of_str
         ldr     r1, [data_seg, #offset_of_x]
         bl      printf
 
-        // x++;
         ldr     r0, [data_seg, #offset_of_x]
         add     r0, r0, #1
-	str     r0, [data_seg, #offset_of_x]
+	    str     r0, [data_seg, #offset_of_x]
 
-        // }
         b       while
+        
+setNum:
+        add r0, data_seg, #offset_of_str
+        mov r1, "\n The digits"
+        bl printf
+        mov r0, #48
+        str r0, [data_seg, #offset_of_x]
+        b while    
+        
+setLower:
+        //mov r0 "\n"
+        //bl printf
+        b done
 
 done:
         .unreq   data_seg
