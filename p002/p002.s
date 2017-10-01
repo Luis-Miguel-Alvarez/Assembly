6dd3d4bf15e0360aacee0449496e1babdfd650a1
Luis Alvarez <alvarelm198@potsdam.edu>
2017-10-01 19:25:49 -0400

    learning to return

--- p002/p002.s
+++ p002/p002.s
@@ -69,16 +69,16 @@ loopB:
 
 loopC:
        ldr r3, [data_seg, #offset_c]
-       cmp r3, #2
+       cmp r3, #3
        bxeq lr
        
        //bl printLoop
        add r0, data_seg, #offset_values
        bl printf
        
-       ldr r1, [data_seg, #offset_a]
-       ldr r2, [data_seg, #offset_b]
-       ldr r3, [data_seg, #offset_c]
+       //ldr r1, [data_seg, #offset_a]
+       //ldr r2, [data_seg, #offset_b]
+       //ldr r3, [data_seg, #offset_c]
        
        add r3, r3, #1
        str r3, [data_seg, #offset_c]
